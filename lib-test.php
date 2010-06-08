5b66e9cab6e844051a13fb238a88d9f057778947
Blaine Lang <blainelang@726382.no-reply.drupal.org>
2010-06-08 17:26:11 +0000

    New revision for testing

--- lib-test.php
+++ lib-test.php
@@ -8,17 +8,25 @@ include_once './' . drupal_get_path('module', 'maestro') . '/maestro.class.php';
 include_once './' . drupal_get_path('module', 'maestro') . '/maestro_tasks.class.php';
 echo "<br>Instantiate the Maestro class";
 $options = array('color1' => 'red');     
-$maestro = Maestro::createMaestroObject(1,$options);    // Initiate the processing of all tasks of type 1.x 
-echo '<br>';
-print_r($maestro);
-echo "<br>Sucessfully launched the Maestro class version: " . $maestro->engine->getVersion();
+$maestro = Maestro::createMaestroObject(1,$options);    // Initiate the processing of all tasks of type 1.x
+if ($maestro) {
+  echo '<br>';
+  print_r($maestro);  
+  echo "<br>Sucessfully launched the Maestro class version: " . $maestro->engine->getVersion();
+} else {
+  echo "<br>Failed to launch a new Maestro class.";
+} 
 
 echo '<hr>';
 echo 'Attempt to launch 2nd instance of engine - version 2 this time.';
 $maestro2 = Maestro::createMaestroObject(2,$options);    // Initiate the processing of all tasks of type 2.x 
-echo '<br>';
-print_r($maestro2);
-echo "<br>Sucessfully launched the Maestro class version: " . $maestro2->engine->getVersion();
+if ($maestro2) {
+  echo '<br>';
+  print_r($maestro2);  
+  echo "<br>Sucessfully launched the Maestro class version: " . $maestro2->engine->getVersion();
+} else {
+  echo "<br>Failed to launch a new Maestro class.";
+}
 
 echo '<hr>';
 echo 'Attempt to execute task.<br>';
