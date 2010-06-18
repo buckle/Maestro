3fd96a3bd796946e6eb58bbf050aa67dbd1a1d28
Randy Kolenko <_randy@704970.no-reply.drupal.org>
2010-06-18 19:26:03 +0000

    Update to save the AND task in cvs.

--- maestro_tasks.class.php
+++ maestro_tasks.class.php
@@ -1,8 +1,8 @@
 <?php
 
 abstract class MaestroTask {
-  public $_properties = NULL;
-  private $_message = NULL;
+  protected $_properties = NULL;
+  protected $_message = NULL;
   public $executionStatus = NULL;
 
   function __construct($properties) {
@@ -109,8 +109,34 @@ class MaestroTaskTypeAnd extends MaestroTask {
   function execute() {
     $msg = 'Execute Task Type: "AND" - properties: ';
     watchdog('maestro',$msg);
+
+    $numComplete = 0;
+    $numIncomplete = 0;
+
+    $query = db_select('maestro_queue', 'a');
+    $query->join('maestro_template_data_next_step', 'b', 'a.template_data_id = b.template_data_to OR a.template_data_id=b.template_data_to_false');
+    $query->addExpression('COUNT(a.id)','templateCount');
+    $query->condition("a.id",$this->_properties->id,"=");
+    $numComplete = $query->execute()->fetchObject();
+
+    $query = db_select('maestro_queue_from', 'a');
+    $query->join('maestro_queue', 'b', 'a.from_queue_id = b.id');
+    $query->addExpression('COUNT(a.id)','processCount');
+    $query->condition(db_and()->condition("a.queue_id",$this->_properties->id,"=")->condition("b.process_id",$this->_properties->process_id,"="));
+    $numIncomplete = $query->execute()->fetchObject();
+
+    // sounds confusing, but if the processCount is greater than the completed ones, we're ok too
+    if ($numIncomplete->processCount == $numComplete->templateCount || $numIncomplete->processCount > $numComplete->templateCount ) {
+        // we have all of the incoming items done for this AND
+        // we can now carry out updating this queue item's information
+        $this->executionStatus = TRUE;
+    } else {
+        // not all the incomings for the AND are done
+        // just here for troubleshooting purposes
+        $this->executionStatus = FALSE;
+    }
+
     $this->setMessage( $msg . print_r($this->_properties, true) . '<br>');
-    $this->executionStatus = TRUE;
     return $this;
   }
 
