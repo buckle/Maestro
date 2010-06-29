78f9d55bb031489bd2c0df4561aebcc261194338
Blaine Lang <blainelang@726382.no-reply.drupal.org>
2010-06-29 13:56:44 +0000

    New revision for testing

---
+++ theme/maestro-taskconsole.tpl.php
@@ -0,0 +1,17 @@
+<?php
+// $Id:
+
+/**
+ * @file
+ * maestro-taskconsole.tpl.php
+ */
+
+?>
+
+<table width="100%">
+<tr>
+<th width="40%">Flow Name</th>
+<th width="40%">Task Name</th>
+<th width="20%">Assigned</th>
+</tr>
+</table>
