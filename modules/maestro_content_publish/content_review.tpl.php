2db589588b5b2274cfda1f943d2d61fe1a4d835b
Blaine Lang <blainelang@726382.no-reply.drupal.org>
2010-08-17 19:33:57 +0000

    Fix for edit node link in the sample content type task - use the drupal l() function.

--- modules/maestro_content_publish/content_review.tpl.php
+++ modules/maestro_content_publish/content_review.tpl.php
@@ -1,7 +1,7 @@
 <div style="margin:5px;padding:10px;border:1px solid #CCC;">
   <form style="margin:0px;">
     <div style="float:left;width:75%;">
-      <div><?php print t('You have a task to Review and Edit the Change Request titled'); ?> <a href="<?php print $edit_content_link; ?>"><?php print $title ?></a>.</div>
+      <div><?php print t('You have a task to Review and Edit the following content'); ?> <span style="padding-left:5px;"> <?php print $edit_content_link; ?></span></div>
     </div>
     <div style="float:right;width:25%;white-space:nowrap">
       <span style="float:right;padding-left:5px;"><input maestro="complete" type="button" value="<?php print t('Complete Task'); ?>"></span>
