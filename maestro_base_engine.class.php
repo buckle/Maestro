d82bbc846b347ce2b80e12997eb75fa89d87dca0
Randy Kolenko <_randy@704970.no-reply.drupal.org>
2011-02-01 15:14:11 +0000

    For issue 1047526 where sql server is unable to process the query, the group by clause was wrong.  Missing fields. Added those fields to the group by clause.

--- maestro_base_engine.class.php
+++ maestro_base_engine.class.php
@@ -327,7 +327,7 @@ include_once('maestro_constants.class.php');
       $query->fields('a', array('tracking_id', 'initiating_pid'));
       $query->fields('b', array('template_name'));
       $query->leftJoin('maestro_template', 'b', 'a.template_id=b.id');
-      $query->groupBy('a.initiating_pid');
+      $query->groupBy(array('a.initiating_pid', 'a.tracking_id','b.template_name'));
       $query->condition('a.tracking_id', $tracking_id, '=');
 
       return $query->execute();
