9442771450b3321a2dd8b53ca804bd9b234af1a1
Blaine Lang <blainelang@726382.no-reply.drupal.org>
2011-02-03 18:47:21 +0000

    A few layout tweaks in the template

--- theme/maestro-outstanding-tasks.tpl.php
+++ theme/maestro-outstanding-tasks.tpl.php
@@ -10,16 +10,16 @@
 <fieldset class="form-wrapper">
   <div style="float: right;">
     <form id="system_tasks_form" action="<?php print url('maestro/outstanding'); ?>" method="POST">
-    <label for="show_system_tasks"><?php print t('Show Hidden Tasks'); ?></label>
-    <input type="checkbox" id="show_system_tasks" name="show_system_tasks" value="1" onchange="document.getElementById('system_tasks_form').submit();" <?php print ($show_system_tasks == 1) ? 'checked="checked"':''; ?>>
+    <div style="float:right;padding-right:5px;padding-top:5px;"><label for="show_system_tasks"><?php print t('Show Hidden Tasks'); ?></label></div>
+    <div style="float:right;padding-right:5px;padding-top:5px;"><input type="checkbox" id="show_system_tasks" name="show_system_tasks" value="1" onchange="document.getElementById('system_tasks_form').submit();" <?php print ($show_system_tasks == 1) ? 'checked="checked"':''; ?>></div>
     </form>
   </div>
-  <table class="sticky-enabled sticky-table">
+  <table class="sticky-enabled sticky-table" style="margin-top:0px;width:100%;">
     <thead class="tableheader-processed">
       <tr>
-        <th><?php print t('Task Name'); ?></th>
-        <th><?php print t('Assigned To'); ?></th>
-        <th style="text-align:right"><?php print t('Operation'); ?></th>
+        <th style="width:45%";><?php print t('Task Name'); ?></th>
+        <th style="width:45%;"><?php print t('Assigned To'); ?></th>
+        <th style="text-align:right;width:10%;"><?php print t('Operation'); ?></th>
       </tr>
     </thead>
 
@@ -32,9 +32,9 @@
       $classname = ((++$i % 2) == 0) ? 'even':'odd';
 ?>
       <tr id="ot_row<?php print $i; ?>" class="<?php print $classname; ?>">
-        <td style="vertical-align: top;"><?php print $task->taskname; ?></td>
-        <td style="vertical-align: top;"><?php print $task->username; ?></td>
-        <td style="text-align:right;vertical-align: top; width: 200px;">
+        <td style="vertical-align:top;"><?php print $task->taskname; ?></td>
+        <td style="vertical-align:top;"><?php print $task->username; ?></td>
+        <td style="text-align:right;vertical-align:top;white-space:nowrap;">
           <?php print l("<img class=\"valigncenter\" src=\"{$maestro_url}/images/taskconsole/reassign.png\">", "maestro/outstanding/reassign/{$task->queue_id}", array('html' => TRUE, 'attributes' => array('title' => t('Reassign this Task'), 'onclick' => "show_reassign(this, '{$task->uid}', '{$assign_types[$task->assign_type]}'); return false;") )); ?>
           <?php print l("<img class=\"valigncenter\" src=\"{$maestro_url}/images/taskconsole/email.png\">", "maestro/outstanding/email/{$task->queue_id}/{$task->uid}", array('html' => TRUE, 'attributes' => array('title' => t('Send a Reminder to Task Owner')) )); ?>
           <?php print l("<img class=\"valigncenter\" src=\"{$maestro_url}/images/taskconsole/trace.png\">", "maestro/trace/0/{$task->process_id}/{$task->queue_id}", array('html' => TRUE, 'attributes' => array('title' => t('Trace this Process')) )); ?>
